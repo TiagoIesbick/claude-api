80f932c804cdd6c8772961e4ef04cdb95a2276cb
TiagoIesbick <tiagoiesbick@gmail.com>
2026-06-30 00:12:54 -0300

    Implement tool and resource functionalities in MCP server and client. Enhance MCPClient methods to list tools, prompts, and read resources, returning appropriate results. Add new tools and resources in MCP server for document handling, including reading, editing, and formatting documents. Introduce error handling for document fetching.

--- mcp/cli_project/mcp_server.py
+++ mcp/cli_project/mcp_server.py
@@ -1,5 +1,6 @@
 from pydantic import Field
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp.prompts import base
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
@@ -13,7 +14,8 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
+
+# Tools
 @mcp.tool(
     name="read_doc_contents",
     description="Read the contents of a document and return it as a string"
@@ -26,7 +28,7 @@ def read_document(
 
     return docs[doc_id]
 
-# TODO: Write a tool to edit a doc
+
 @mcp.tool(
     name="edit_document",
     description="Edit a document by replacing a string in the documents content with a new string"
@@ -41,9 +43,46 @@ def edit_document(
 
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+
+# Resources
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
+
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type=""
+)
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f'Doc with id {doc_id} not found')
+    return docs[doc_id]
+
 # TODO: Write a prompt to rewrite a doc in markdown format
+@mcp.prompt(
+    name="format",
+    description="Rewrites the contents of the document in Markdown format."
+)
+def format_document(
+    doc_id: str = Field(description="Id of the document to format")
+) -> list[base.Message]:
+    prompt = f'''
+    Your goal is to reformat a document to be written with markdown syntax.
+
+    The id of the document you need to reformat is:
+    <document_id>
+    {doc_id}
+    </document_id>
+
+    Add in headers, bullet points, tables, etc as necessary. Feel free to add in the extra formatting.
+    Use the 'edit_document' tool to edit the document. After the document has been reformatted...
+    '''
+
+    return [base.UserMessage(prompt)]
 # TODO: Write a prompt to summarize a doc
 
 
